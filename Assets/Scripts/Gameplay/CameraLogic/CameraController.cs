3c724e5c8f46ac21b3b0afb372730a4641feaced
agarosha <agaronoganisyan@gmail.com>
2023-12-20 12:38:59 +0400

    camera logic

---
+++ Assets/Scripts/Gameplay/CameraLogic/CameraController.cs
@@ -0,0 +1,46 @@
+using Infrastructure;
+using Infrastructure.ServiceLogic;
+using UnityEngine;
+
+namespace Gameplay.CameraLogic
+{
+    public class CameraController : MonoBehaviour , IInitializable
+    {
+        private IInputService _inputService;
+
+        [SerializeField] private Transform _transform;
+        [SerializeField] private Transform _targetTransform;
+        
+        [SerializeField] private float _rotationSpeed;
+        private float _horizontalRotation;
+        private void Start()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            _inputService = ServiceLocator.Get<IInputService>();
+            _inputService.OnRotationInputReceived += HandleRotation;
+        }
+
+        private void LateUpdate()
+        {
+            HandleMovement();
+        }
+
+        void HandleMovement()
+        {
+            _transform.position = _targetTransform.position;
+        }
+
+        void HandleRotation(Vector2 rotation)
+        {
+            //_horizontalRotation = delta.x * Time.deltaTime * _rotationSpeed;
+            _horizontalRotation = rotation.x * _rotationSpeed;
+            
+            _transform.localEulerAngles = new Vector3(0, _horizontalRotation, 0);
+            //transform.Rotate(Vector3.up, _horizontalRotation);
+        }
+    }
+}
